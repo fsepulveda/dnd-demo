7a8d0959cbf1f930ff64e6db4218cdd1f658c1ef
Felipe Sepulveda <felipe.sepulveda@masterbase.com>
2020-04-24 12:10:26 -0400

    Fix

--- api/tasks.js
+++ api/tasks.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", async (req, res, next) => {
-  return res.json({
+  return res.status(200).json({
     tasks: [
       { id: 1, title: "one" },
       { id: 2, title: "two" },
